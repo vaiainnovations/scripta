3b5543fe7ada4a744e151307ef9074ce0c2a2aa9
Luca <luca.g@protonmail.ch>
2022-07-04 18:05:55 +0200

    deps: desmjs & walletconnect

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,34 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { defineNuxtConfig } from "nuxt";
 import eslintPlugin from "vite-plugin-eslint";
+
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+const path = require("path");
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   vite: {
-    plugins: [eslintPlugin()]
+    plugins: [eslintPlugin()],
+    resolve: {
+      extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+      alias: {
+        "@": path.resolve(__dirname, "/src"),
+        process: "process/browser",
+        stream: "stream-browserify"
+      }
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+          global: "globalThis"
+        },
+        // Enable esbuild polyfill plugins
+        plugins: [
+          NodeGlobalsPolyfillPlugin()
+        ]
+      }
+    }
   },
   alias: {
     "@vue/devtools-api": "@vue/devtools-api"
