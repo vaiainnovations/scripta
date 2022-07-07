cdddfd0dcf41a481da8ffc73792a94a4fb066b91
Valendrew <andrea.valente99@gmail.com>
2022-07-07 15:43:30 +0200

    fix: navbar profile text has gradient color

--- components/NavBar/NavBarProfile.vue
+++ components/NavBar/NavBarProfile.vue
@@ -8,12 +8,20 @@
     <!-- The button to write new article appears on larger screen (>= 768 px) -->
     <!--  TODO if better md of lg breakpoint to show the name-->
     <div class="hidden md:block">
-      <div class="flex h-10 w-44 flex-row items-center justify-center rounded-lg border border-primary-text-light">
-        <!-- TODO gradient color doesn't work even though it seems correct -->
-        <button type="button" class=" text-white bg-gradient-to-r from-primary-light to-[#5BFF6C] bg-clip-text text-2xl font-semibold">
+      <div class="gradient-text flex h-10 w-44 flex-row items-center justify-center rounded-lg border border-primary-text-light">
+        <button
+          type="button"
+          class="bg-gradient-to-r from-primary-light to-[#5BFF6C] bg-clip-text text-2xl font-semibold"
+        >
           Write
         </button>
       </div>
     </div>
   </div>
 </template>
+
+<style>
+div.gradient-text {
+  -webkit-text-fill-color: transparent;
+}
+</style>
