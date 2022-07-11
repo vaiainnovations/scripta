bbc91f31e5cbc8f7784babe444caf837a8712748
Valendrew <andrea.valente99@gmail.com>
2022-07-11 14:55:28 +0200

    added edit profile button

--- components/Profile/ProfileUser.vue
+++ components/Profile/ProfileUser.vue
@@ -1,21 +1,33 @@
 <template>
-  <div class="flex flex-col justify-start items-center px-10 gap-y-3.5 h-fit">
-    <img src="/img/author_pic.png" class="h-32 w-32 object-contain border-2 shadow-md rounded-full border-[#EDEEFF]">
-    <p class="text-3xl text-center font-bold">
-      John
-    </p>
-    <p class="text-xl text-center">
-      @username
-    </p>
-    <div class="min-h-40 h-40 overflow-hidden">
-      <p class="font-light text-xs text-center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus ante vel nisl consequat tincidunt. Vestibulum et diam nisi. Proin a
-        justo sit amet libero cursus porttitor. Nam lectus enim, volutpat vitae tellus suscipit, dapibus sollicitudin nibh. Maecenas accumsan, massa
-        quis mollis posuere, purus mi scelerisque lorem, id porta felis arcu fermentum ante. Curabitur malesuada maximus metus, in tincidunt mi
-        scelerisque a. Sed et finibus dui. Mauris euismod at nisi ut dapibus. Mauris pharetra ligula nec pharetra pharetra. Vestibulum metus nibh,
-        malesuada sed facilisis vel, dapibus dignissim nisl. Lorem Ipsum Maecenas sed tortor dapibus, finibus ipsum eu, condimentum lorem. Cras varius
-        tincidunt cursus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+  <div class="lg:relative">
+    <div class="absolute w-full flex flex-col items-end justify-start mt-7 lg:mt-0 right-8 lg:right-2 gap-y-32 lg:gap-y-36">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        class="w-5 h-5"
+      >
+        <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+      </svg>
+      <img src="/icons/broken/edit.svg" class="w-4 h-4">
+    </div>
+    <div class="flex flex-col justify-start items-center px-10 gap-y-3.5 h-fit mt-14 lg:mt-2">
+      <img src="/img/author_pic.png" class="h-32 w-32 object-contain border-2 shadow-md rounded-full border-[#EDEEFF]">
+      <p class="text-3xl text-center font-bold">
+        John
       </p>
+      <p class="text-xl text-center">
+        @username
+      </p>
+      <div class="min-h-40 h-40 overflow-hidden">
+        <p class="font-light text-xs text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus ante vel nisl consequat tincidunt. Vestibulum et diam nisi. Proin a
+          justo sit amet libero cursus porttitor. Nam lectus enim, volutpat vitae tellus suscipit, dapibus sollicitudin nibh. Maecenas accumsan, massa
+          quis mollis posuere, purus mi scelerisque lorem, id porta felis arcu fermentum ante. Curabitur malesuada maximus metus, in tincidunt mi
+          scelerisque a. Sed et finibus dui. Mauris euismod at nisi ut dapibus. Mauris pharetra ligula nec pharetra pharetra. Vestibulum metus nibh,
+          malesuada sed facilisis vel, dapibus dignissim nisl. Lorem Ipsum Maecenas sed tortor dapibus, finibus ipsum eu, condimentum lorem. Cras varius
+          tincidunt cursus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        </p>
+      </div>
     </div>
   </div>
 </template>
