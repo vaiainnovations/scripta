0e5ff46acc4b1166053541631697049c3b7705b3
g-luca <luca.g@protonmail.ch>
2022-07-25 16:50:04 +0200

    wip: ipfs

--- components/Articles/ArticlesCreate/ArticlesCreateButtons.vue
+++ components/Articles/ArticlesCreate/ArticlesCreateButtons.vue
@@ -1,13 +1,93 @@
 <template>
   <div class="grid grid-cols-2 lg:grid-cols-6 w-full gap-y-4 gap-x-6 lg:gap-x-4 xl:gap-x-10">
-    <button type="button" class="p-1 col-span-1 rounded-xl text-[#FFFFFF] bg-primary-text text-xl font-medium">
+    <button
+      type="button"
+      class="p-1 col-span-1 rounded-xl text-[#FFFFFF] bg-primary-text text-xl font-medium"
+    >
       Save Draft
     </button>
-    <button type="button" class="col-span-1 rounded-xl text-[#FFFFFF] bg-danger text-xl font-medium">
+    <button
+      type="button"
+      class="col-span-1 rounded-xl text-[#FFFFFF] bg-danger text-xl font-medium"
+    >
       Delete
     </button>
-    <button type="button" class="col-span-2 lg:col-start-5 rounded-xl text-[#FFFFFF] bg-primary text-xl font-medium">
+    <button
+      type="button"
+      class="col-span-2 lg:col-start-5 rounded-xl text-[#FFFFFF] bg-primary text-xl font-medium"
+      @click="publish()"
+    >
       Publish
     </button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { MsgCreatePostEncodeObject } from "@desmoslabs/desmjs";
+import Long from "long";
+import { v4 as uuidv4 } from "uuid";
+import { Media } from "@desmoslabs/desmjs-types/desmos/posts/v2/models";
+import { useAccountStore } from "~~/core/store/AccountStore";
+import { useDraftStore } from "~~/core/store/DraftStore";
+import { useTransactionStore } from "~~/core/store/TransactionStore";
+import { useIpfsStore } from "~~/core/store/IpfsStore";
+
+async function publish () {
+  const draftStore = useDraftStore();
+  const extId = uuidv4();
+
+  const msgCreatePost: MsgCreatePostEncodeObject = {
+    typeUrl: "/desmos.posts.v2.MsgCreatePost",
+    value: {
+      subspaceId: Long.fromNumber(8),
+      externalId: extId,
+      attachments: [],
+      author: useAccountStore().address,
+      text: draftStore.title,
+      sectionId: 2,
+      tags: [],
+      conversationId: Long.fromNumber(0),
+      referencedPosts: [],
+      replySettings: 1
+    }
+  };
+
+  // upload the post (without CID attachment) to IPFS, get the CID
+  const ipfsPost: any = {
+    ...msgCreatePost.value,
+    text: {
+      title: draftStore.title,
+      subtitle: draftStore.subtitle,
+      content: draftStore.content
+    }
+  };
+  const postCid = await useIpfsStore().uploadPost(JSON.stringify(ipfsPost));
+  const postIpfsUrl = `https://cloudflare-ipfs.com/ipfs/${postCid}`;
+  msgCreatePost.value.attachments = [{
+    typeUrl: "/desmos.posts.v2.Media",
+    value: Media.encode({
+      mimeType: "text/plain",
+      uri: postIpfsUrl
+    }).finish()
+  }];
+  useTransactionStore().push(msgCreatePost);
+  const signedBytes = await useTransactionStore().execute();
+
+  /*  const res = await (
+    await fetch("http://192.168.1.108:4000/v1/test3", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        signedPost: Buffer.from(signedBytes).toString("base64"),
+        extId: msgCreatePost.value.externalId,
+        author: msgCreatePost.value.author,
+        sectionId: msgCreatePost.value.sectionId,
+        text: msgCreatePost.value.text
+      })
+    })
+  ).json(); */
+  console.log(signedBytes);
+}
+</script>
---
+++ core/store/IpfsStore.ts
@@ -0,0 +1,27 @@
+import { defineStore } from "pinia";
+import { create } from "ipfs-http-client";
+import { registerModuleHMR } from ".";
+
+export const useIpfsStore = defineStore({
+  id: "IpfsStore",
+  state: () => ({
+    client: create({ url: "https://ipfs.infura.io:5001" })
+  }),
+  actions: {
+    /**
+     * Upload Post content to IPFS
+     * @param content string of the post content to upload
+     * @returns CID of the uploaded content, empty string otherwise
+    */
+    async uploadPost (content: string): Promise<string> {
+      try {
+        return (await this.client.add(content)).path;
+      } catch (e) {
+        return "";
+      }
+    }
+  }
+});
+
+// Register the store to enable HMR
+registerModuleHMR(useIpfsStore);
