085ffb63a295542d98d3b08ac480c24dfc5c72b8
g-luca <luca.g@protonmail.ch>
2022-07-30 16:46:30 +0200

    feat: navbar UX improvements

--- components/NavBar/NavBarProfile.vue
+++ components/NavBar/NavBarProfile.vue
@@ -1,22 +1,55 @@
 <template>
-  <NuxtLink to="/profile" class="w-fit">
-    <!-- The profile icon is showed only on mobile screens (< 768 px) -->
-    <div class="block md:hidden">
-      <img src="/icons/linear/profile-circle.svg" class="w-11 object-fill">
-    </div>
+  <div class="flex w-fit">
     <!-- The button to write new article appears on larger screen (>= 768 px) -->
-    <div class="hidden md:block">
-      <div class="gradient-text flex h-10 w-44 flex-row items-center justify-center rounded-lg border border-primary-text-light">
-        <button
-          type="button"
-          class="bg-gradient-to-r from-primary-light to-[#5BFF6C] bg-clip-text text-2xl font-semibold"
-        >
-          Write
-        </button>
+    <NuxtLink
+      to="/new"
+      class="flex-1 w-fit mr-4 my-auto"
+    >
+      <div class="hidden md:block">
+        <div class="gradient-text flex h-10 flex-row items-center justify-center rounded-lg border border-primary-text-light sm:px-5 md:px-10 mr-3">
+          <button
+            type="button"
+            class="bg-gradient-to-r from-primary-light to-[#5BFF6C] bg-clip-text text-2xl font-semibold"
+          >
+            Write
+          </button>
+        </div>
       </div>
-    </div>
-  </NuxtLink>
+    </NuxtLink>
+
+    <NuxtLink
+      to="/profile"
+      class="flex-none"
+    >
+      <div class="">
+        <img
+          :src="userPic"
+          class="w-11 h-11 object-cover rounded-full"
+        >
+      </div>
+    </NuxtLink>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { useAccountStore } from "~~/core/store/AccountStore";
+const userPic = ref("/icons/linear/profile-circle.svg");
+if (process.client) {
+  useAccountStore().$subscribe(() => {
+    updateUserPic();
+  });
+  updateUserPic();
+}
+
+function updateUserPic () {
+  if (!useAccountStore().profile) {
+    return;
+  }
+  if (useAccountStore().profile.pictures) {
+    userPic.value = useAccountStore().profile.pictures.profile;
+  }
+}
+</script>
 
 <style>
 div.gradient-text {
