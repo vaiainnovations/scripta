fc586913ffc45e577338c2a3052bbbe503665c39
g-luca <luca.g@protonmail.ch>
2022-08-02 18:42:10 +0200

    fix: improved logout button

--- components/Profile/ProfileBalance.vue
+++ components/Profile/ProfileBalance.vue
@@ -4,13 +4,25 @@
       Wallet
     </p>
     <div class="flex flex-row gap-x-3">
-      <img src="/svg/wallet/dpm/logo.svg" class="h-5 w-5 object-contain">
+      <img
+        src="/svg/wallet/dpm/logo.svg"
+        class="h-5 w-5 object-contain"
+      >
       <p class="text-xl font-medium">
         {{ useAccountStore().balance }} {{ useDesmosStore().coinDenom.toUpperCase() }}
       </p>
     </div>
-    <button class="px-1 py-0.5 bg-danger rounded-2xl" @click="logout()">
-      Logout
+    <button
+      class="px-1 py-0.5 hover:text-danger rounded-2xl w-1/3 md:w-1/4 pt-4"
+      @click="logout()"
+    >
+      <div class="flex">
+        <img
+          src="/icons/linear/logout.svg"
+          class="h-5 w-5 object-contain mr-4 stroke-background my-auto"
+        >
+        Logout
+      </div>
     </button>
   </div>
 </template>
