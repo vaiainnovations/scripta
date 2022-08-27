9d5bbb7ba7d2d970c6da1760bfd634382beac291
g-luca <luca.g@protonmail.ch>
2022-08-27 12:11:13 +0200

    wip: settings save & loading skeleton

--- components/Settings/SettingsContainer.vue
+++ components/Settings/SettingsContainer.vue
@@ -10,7 +10,7 @@
       <div class="mb-12 bg-background">
         <div class="flex py-4 md:pt-3 fixed w-full bg-background z-50">
           <div class="flex-1 my-auto">
-            <NuxtLink to="/profile">
+            <NuxtLink v-if="!isUpdating" to="/profile">
               <div class="flex">
                 <img :src="'/icons/bold/arrow-left.svg'">
                 <p class="text-gray-dark">
@@ -27,37 +27,53 @@
         </div>
       </div>
 
-      <div class="p-4 py-6">
+      <div v-if="isUpdating" class="py-10 w-full">
+        <div class="flex flex-col pb-16">
+          <SkeletonSettings v-for="n in 3" :key="n" />
+        </div>
+        <div class="mx-auto text-center w-full rounded-2xl mb-32 md:mb-0">
+          <p class="text-lg text-primary-text/70 font-medium p-3">
+            Updating
+          </p>
+          <img class="animate-pulse mx-auto w-10" src="/svg/spinner/dots.svg">
+        </div>
+      </div>
+
+      <div v-else class="p-4 py-6">
         <!-- Privacy Settings -->
         <div class="py-4">
           <h1 class="font-semibold text-gray-dark">
             Privacy
           </h1>
           <div class="mt-2">
-            <SettingsToggle v-model="hasAcceptedPrivacy" class="py-4 mb-4 bg-background-alt rounded-xl">
+            <SettingsToggle v-model="hasAcceptedPrivacy" class="py-4 mb-4 bg-background-alt rounded-xl" @click="changePrivacySettings()">
               <h1 class="font-bold text-xl">
                 Privacy & Terms
               </h1>
               <p class="font-light">
-                Agree with privacy and terms of the Scripta platform.
+                Agree with <a href="" target="_blank" class="underline hover:text-primary-light">Privacy and Terms</a> of the Scripta platform.
               </p>
             </SettingsToggle>
-            <SettingsToggle v-model="hasAcceptedAdvertisement" class="py-4 mb-4 bg-background-alt rounded-xl">
+            <SettingsToggle v-model="hasAcceptedAdvertisement" class="py-4 mb-4 bg-background-alt rounded-xl" @click="changePrivacySettings()">
               <h1 class="font-bold text-xl">
                 Advertisement
               </h1>
               <p class="font-light">
-                Agree with advertisement policy of the Scripta platform.
+                Agree with <a href="" target="_blank" class="underline hover:text-primary-light">Advertisement Policy</a> of the Scripta platform.
               </p>
             </SettingsToggle>
-            <SettingsToggle v-model="hasAcceptedCookies" class="py-4 mb-4 bg-background-alt rounded-xl">
+            <SettingsToggle v-model="hasAcceptedCookies" class="py-4 mb-4 bg-background-alt rounded-xl" @click="changePrivacySettings()">
               <h1 class="font-bold text-xl">
                 Cookies
               </h1>
               <p class="font-light">
-                Agree with cookies policy of the Scripta platform.
+                Agree with <a href="" target="_blank" class="underline hover:text-primary-light">Cookies Policy</a> of the Scripta platform.
               </p>
             </SettingsToggle>
+
+            <button v-if="arePrivacySettingsChanged" class="rounded-lg bg-primary-light/80 hover:bg-primary-light text-background-alt w-full py-2" @click="savePrivacySettings()">
+              Save Changes
+            </button>
           </div>
         </div>
 
@@ -106,29 +122,62 @@
 
 <script lang="ts" setup>
 import { useAccountStore } from "~~/core/store/AccountStore";
+import { useBackendStore } from "~~/core/store/BackendStore";
 import { useDesmosStore } from "~~/core/store/DesmosStore";
+
+const isUpdating = ref(false);
 
 const hasAcceptedPrivacy = ref(true);
 const hasAcceptedAdvertisement = ref(false);
 const hasAcceptedCookies = ref(false);
-const hasAuthzAuthorization = ref(useAccountStore().authz.hasAuthz);
+const arePrivacySettingsChanged = ref(false);
+
+const hasAuthzAuthorization = ref(false);
+
+onMounted(() => {
+  updateSettingsValues();
+});
 
 // subscribe to changes in the account store, the first call to the Account endpoint is async, so it is necessary
 useAccountStore().$subscribe(() => {
-  hasAcceptedPrivacy.value = true;
-  hasAcceptedAdvertisement.value = false;
-  hasAcceptedCookies.value = false;
-  hasAuthzAuthorization.value = useAccountStore().authz.hasAuthz;
+  updateSettingsValues();
 });
 
+function changePrivacySettings () {
+  arePrivacySettingsChanged.value = true;
+}
+
+async function savePrivacySettings () {
+  isUpdating.value = true;
+
+  const success = await useBackendStore().fetch(`${useBackendStore().apiUrl}user/${useAccountStore().address}`, "PUT", {
+    "Content-Type": "application/json"
+  }, JSON.stringify({
+    sectionId: useAccountStore().sectionId,
+    privacyNotifications: hasAcceptedPrivacy,
+    privacyTracking: hasAcceptedCookies
+  }));
+  console.log(success);
+  arePrivacySettingsChanged.value = false;
+  isUpdating.value = false;
+}
+
+function updateSettingsValues () {
+  hasAcceptedPrivacy.value = useAccountStore().settings.hasAcceptedPrivacy;
+  hasAcceptedAdvertisement.value = useAccountStore().settings.hasAcceptedAdvertisement;
+  hasAcceptedCookies.value = useAccountStore().settings.hasAcceptedCookies;
+  hasAuthzAuthorization.value = useAccountStore().authz.hasAuthz;
+}
+
 async function handleAuthzAuthorizationChange () {
+  isUpdating.value = true;
   if (hasAuthzAuthorization.value) {
-    console.log("grant authorization");
     const success = await useAccountStore().grantAuthorizations();
-    console.log("grant authorization", success);
+    hasAuthzAuthorization.value = success;
   } else {
     const success = await useAccountStore().revokeAuthorizations();
-    console.log("revoke authorization", success);
+    hasAuthzAuthorization.value = !success;
   }
+  isUpdating.value = false;
 }
 </script>
---
+++ components/Skeleton/SkeletonSettings.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full p-6">
+    <div class="bg-background-alt mx-auto p-2 sm:p-4 rounded-2xl shadow-lg flex flex-col sm:flex-row gap-5 select-none">
+      <div class="flex flex-col flex-1 gap-5 sm:p-2">
+        <div class="flex flex-1 flex-col gap-3">
+          <div class="flex">
+            <div class="bg-background w-5/6 animate-pulse h-14 md:h-24 rounded-2xl" />
+            <div class="bg-background w-1/6 animate-pulse h-10 md:h-14 rounded-2xl ml-5 my-auto" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
