fedb2d0e8f99deb0f2bba9cd0ffced2103db5e3b
g-luca <luca.g@protonmail.ch>
2022-08-29 11:49:17 +0200

    wip: comments authz

--- components/Articles/Comments/CommentsWriteContainer.vue
+++ components/Articles/Comments/CommentsWriteContainer.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
+import { Buffer } from "buffer";
 import { MsgCreatePostEncodeObject } from "@desmoslabs/desmjs";
 import { PostReferenceType } from "@desmoslabs/desmjs-types/desmos/posts/v2/models";
 import Long from "long";
 import { v4 as uuidv4 } from "uuid";
 import { useAccountStore } from "~~/core/store/AccountStore";
+import { useBackendStore } from "~~/core/store/BackendStore";
 import { useDesmosStore } from "~~/core/store/DesmosStore";
 
 interface Props {
@@ -23,7 +25,7 @@ if (process.client) {
   }
 }
 
-function postComment () {
+async function postComment () {
   const { $useTransaction } = useNuxtApp();
   const msgCreateComment: MsgCreatePostEncodeObject = {
     typeUrl: "/desmos.posts.v2.MsgCreatePost",
@@ -45,7 +47,41 @@ function postComment () {
     }
   };
   isCommentPublishing.value = true;
-  $useTransaction().push(msgCreateComment);
+  let signedBytes = new Uint8Array();
+  try {
+    if (!useAccountStore().authz.hasAuthz) {
+      signedBytes = await $useTransaction().directSign([msgCreateComment]);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  if (!signedBytes) {
+    return;
+  }
+
+  const res = (await (
+    await useBackendStore().fetch(
+      `${useBackendStore().apiUrl}/comments/${msgCreateComment.value.externalId}`,
+      "POST",
+      {
+        "Content-Type": "application/json"
+      },
+      JSON.stringify({
+        signedPost: (signedBytes) ? Buffer.from(signedBytes).toString("base64") : "",
+        externalId: msgCreateComment.value.externalId,
+        author: useAccountStore().address,
+        sectionId: msgCreateComment.value.sectionId,
+        text: msgCreateComment.value.text,
+        referencedPosts: msgCreateComment.value.referencedPosts
+      })
+    )
+  ).json()) as any;
+  if (res.code === 0) {
+    comment.value = "";
+    isCommentPublishing.value = false;
+    emit("newComment");
+  }
 
   $useTransaction().$subscribe(() => {
     if ($useTransaction().queue.length === 0) {
