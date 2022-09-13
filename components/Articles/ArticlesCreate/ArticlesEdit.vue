13415589e72b8a2ab633dbf362d5e653c5a184bf
g-luca <luca.g@protonmail.ch>
2022-09-13 15:59:52 +0200

    feat: improved articles write UI

--- components/Articles/ArticlesCreate/ArticlesEdit.vue
+++ components/Articles/ArticlesCreate/ArticlesEdit.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="bg-background w-full flex flex-col items-center justify-start px-4 gap-y-3.5 py-3.5 lg:px-32 lg:py-20 lg:gap-y-2.5">
+  <div class="bg-background min-h-screen w-full flex flex-col items-center justify-start px-4 gap-y-3.5 py-3.5 lg:px-32 lg:py-20 lg:gap-y-2.5">
     <div class="grid grid-cols-2 self-start justify-start w-full items-center">
       <ArticlesEditorButtonBackRouting class="col-span-2 lg:col-span-1" />
     </div>
     <!-- Tags -->
-    <ArticlesCreateTags />
+    <ArticlesCreateTags class="pt-6" />
     <!-- Input form -->
     <ArticlesCreateInput />
     <!-- Submit buttons -->
-    <ArticlesEditButtons />
+    <ArticlesEditButtons class="mt-6" />
   </div>
 </template>
 
