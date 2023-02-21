e435ce4b6ed81e17526178ed9c19d9e8c1e78317
g-luca <luca.g@protonmail.ch>
2023-02-21 16:58:54 +0100

    feat: Article Tip UX improvements

--- components/Articles/Tips/TipsOverlay.vue
+++ components/Articles/Tips/TipsOverlay.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-screen top-0 left-0 h-screen fixed flex align-middle items-center z-10">
     <div class="w-full sm:w-1/2 md:w-1/3 xl:w-1/4 p-2 mx-auto rounded-2xl shadow-xl bg-background-alt dark:bg-gray-800 ring-opacity-5 z-30 m-2">
-      <div class="py-1 p-5" role="menu" aria-orientation="vertical">
+      <div v-if="tipStatus === 'NONE'" class="py-1 p-5" role="menu" aria-orientation="vertical">
         <div class="text-center mt-2">
           <!-- <img src="/icons/bold/info-circle.svg" class="w-12 mx-auto"> -->
           <h2 class="text-4xl font-extrabold py-2 text-primary-text">
@@ -49,6 +49,35 @@
           </div>
         </div>
       </div>
+      <div v-if="tipStatus === 'PENDING'" class="text-center">
+        <img
+          src="/svg/spinner/dots.svg"
+          class="h-8 p-2 my-4 mx-auto object-contain"
+        >
+        <h4 class="text-xl font-medium">
+          Tipping...
+        </h4>
+      </div>
+      <div v-if="tipStatus === 'SUCCESS'" class="text-center">
+        <img
+          src="/icons/linear/tick-circle.svg"
+          class="h-16 p-2 mx-auto object-contain"
+        >
+        <h4 class="text-xl font-medium">
+          Thank you for your tip!
+        </h4>
+        <p>We're sure that @{{ nickname }} will appreciate your kind gesture.</p>
+      </div>
+      <div v-if="tipStatus === 'ERROR'" class="text-center">
+        <img
+          src="/icons/linear/close-circle.svg"
+          class="h-16 p-2 mx-auto object-contain"
+        >
+        <h4 class="text-xl font-medium">
+          Ops, something went wrong
+        </h4>
+        <p>Please retry later</p>
+      </div>
     </div>
     <div class="w-screen top-0 left-0 h-screen fixed bg-gray/30 flex align-middle items-center z-10" @click="emit('closeTipsOverlay')" />
   </div>
@@ -67,6 +96,7 @@ const emit = defineEmits(["closeTipsOverlay"]);
 
 const tipAmounts = [5, 10, 50, 100];
 const tipAmount = ref(1);
+const tipStatus = ref("NONE" as "NONE" | "PENDING" | "SUCCESS" | "ERROR");
 
 const nickname = ref(props.author);
 nickname.value = (await useUserStore().getUser(props.author, true)).dtag;
@@ -78,7 +108,7 @@ function setTipAmount (amount:number) {
   tipAmount.value = amount;
 }
 
-function sendTip () {
+async function sendTip () {
   const { $useTransaction } = useNuxtApp();
   const msgSend: MsgSendEncodeObject = {
     typeUrl: "/cosmos.bank.v1beta1.MsgSend",
@@ -93,9 +123,14 @@ function sendTip () {
       toAddress: props.author
     }
   };
-  $useTransaction().directTx([msgSend], [{
+  tipStatus.value = "PENDING";
+  const success = await $useTransaction().directTx([msgSend], [{
     scriptaOp: "MsgSend"
   }], true);
-  emit("closeTipsOverlay");
+  tipStatus.value = success !== false ? "SUCCESS" : "ERROR";
+  setTimeout(() => {
+    tipStatus.value = "NONE";
+    emit("closeTipsOverlay");
+  }, 5000);
 }
 </script>
