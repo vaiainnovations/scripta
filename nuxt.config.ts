801baed2e24f2a04d24b4dc4eec4040d3bc5d58d
g-luca <luca.g@protonmail.ch>
2023-03-21 10:51:58 +0100

    fix: nuxt config on dev mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,8 @@
-/* Build */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import eslintPlugin from "vite-plugin-eslint";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 import builtins from "rollup-plugin-node-builtins";
-import inject from "@rollup/plugin-inject";
 const path = require("path");
 const pjson = require("./package.json");
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
@@ -66,35 +64,41 @@ export default defineNuxtConfig({
     plugins: [eslintPlugin()],
     resolve: {
       extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
-      alias: {
-        "@": path.resolve(__dirname, "/src"),
-        util: "rollup-plugin-node-polyfills/polyfills/util",
-        sys: "util",
-        events: "rollup-plugin-node-polyfills/polyfills/events",
-        stream: "rollup-plugin-node-polyfills/polyfills/stream",
-        path: "rollup-plugin-node-polyfills/polyfills/path",
-        querystring: "rollup-plugin-node-polyfills/polyfills/qs",
-        punycode: "rollup-plugin-node-polyfills/polyfills/punycode",
-        url: "rollup-plugin-node-polyfills/polyfills/url",
-        http: "rollup-plugin-node-polyfills/polyfills/http",
-        https: "rollup-plugin-node-polyfills/polyfills/http",
-        os: "rollup-plugin-node-polyfills/polyfills/os",
-        assert: "rollup-plugin-node-polyfills/polyfills/assert",
-        constants: "rollup-plugin-node-polyfills/polyfills/constants",
-        _stream_duplex: "rollup-plugin-node-polyfills/polyfills/readable-stream/duplex",
-        _stream_passthrough: "rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough",
-        _stream_readable: "rollup-plugin-node-polyfills/polyfills/readable-stream/readable",
-        _stream_writable: "rollup-plugin-node-polyfills/polyfills/readable-stream/writable",
-        _stream_transform: "rollup-plugin-node-polyfills/polyfills/readable-stream/transform",
-        timers: "rollup-plugin-node-polyfills/polyfills/timers",
-        console: "rollup-plugin-node-polyfills/polyfills/console",
-        vm: "rollup-plugin-node-polyfills/polyfills/vm",
-        zlib: "rollup-plugin-node-polyfills/polyfills/zlib",
-        tty: "rollup-plugin-node-polyfills/polyfills/tty",
-        domain: "rollup-plugin-node-polyfills/polyfills/domain",
-        buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
-        process: "rollup-plugin-node-polyfills/polyfills/process-es6"
-      }
+      alias: mode === "production"
+        ? {
+          "@": path.resolve(__dirname, "/src"),
+          util: "rollup-plugin-node-polyfills/polyfills/util",
+          sys: "util",
+          events: "rollup-plugin-node-polyfills/polyfills/events",
+          stream: "rollup-plugin-node-polyfills/polyfills/stream",
+          path: "rollup-plugin-node-polyfills/polyfills/path",
+          querystring: "rollup-plugin-node-polyfills/polyfills/qs",
+          punycode: "rollup-plugin-node-polyfills/polyfills/punycode",
+          url: "rollup-plugin-node-polyfills/polyfills/url",
+          http: "rollup-plugin-node-polyfills/polyfills/http",
+          https: "rollup-plugin-node-polyfills/polyfills/http",
+          os: "rollup-plugin-node-polyfills/polyfills/os",
+          assert: "rollup-plugin-node-polyfills/polyfills/assert",
+          constants: "rollup-plugin-node-polyfills/polyfills/constants",
+          _stream_duplex: "rollup-plugin-node-polyfills/polyfills/readable-stream/duplex",
+          _stream_passthrough: "rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough",
+          _stream_readable: "rollup-plugin-node-polyfills/polyfills/readable-stream/readable",
+          _stream_writable: "rollup-plugin-node-polyfills/polyfills/readable-stream/writable",
+          _stream_transform: "rollup-plugin-node-polyfills/polyfills/readable-stream/transform",
+          timers: "rollup-plugin-node-polyfills/polyfills/timers",
+          console: "rollup-plugin-node-polyfills/polyfills/console",
+          vm: "rollup-plugin-node-polyfills/polyfills/vm",
+          zlib: "rollup-plugin-node-polyfills/polyfills/zlib",
+          tty: "rollup-plugin-node-polyfills/polyfills/tty",
+          domain: "rollup-plugin-node-polyfills/polyfills/domain",
+          buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
+          process: "rollup-plugin-node-polyfills/polyfills/process-es6"
+        }
+        : {
+          process: "rollup-plugin-node-polyfills/polyfills/process-es6",
+          stream: "stream-browserify",
+          util: "util"
+        }
     },
     optimizeDeps: {
       esbuildOptions: {
