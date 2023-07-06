8cd26a910e5e6ee6212f02ba448a07bb85ee9350
g-luca <luca.g@protonmail.ch>
2023-07-06 13:20:09 +0200

    fix: user follow remove UI index

--- components/Profile/ProfileUserFollowings.vue
+++ components/Profile/ProfileUserFollowings.vue
@@ -2,7 +2,7 @@
   <span>
     <div class="flex flex-col justify-start items-center pt-10 px-10 lg:px-20 2xl:px-32 gap-y-3.5">
       <span v-if="useAccountStore().follows.length > 0" class="w-full">
-        <div v-for="(address, i) in useAccountStore().follows" :key="i" class="w-full mb-3">
+        <div v-for="address in useAccountStore().follows" :key="address" class="w-full mb-3">
           <ProfileUserFollowing :address="address" />
         </div>
       </span>
